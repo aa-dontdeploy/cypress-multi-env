1111b69f50c77af569acb4dfb13060a2ab277769
aa-dontdeploy <github@dontdeploy.com>
2024-10-20 13:55:08 +0100

    change cypress env key. handle invalid env value.

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,15 @@ const fs = require("fs");
 module.exports = defineConfig({
 	e2e: {
 		setupNodeEvents(on, config) {
-			const envToUse = config.env.CYPRESS_ENV || "development";
+			const envToUse = config.env.PROJECT_ENV || "development";
 			const envConfig = JSON.parse(fs.readFileSync(`cypress-env-config.json`));
+
+			if (!envConfig[envToUse]) {
+				throw new Error(
+					`Invalid PROJECT_ENV value: ${envToUse}. Please provide a valid environment.`
+				);
+			}
+
 			return envConfig[envToUse];
 		},
 	},
